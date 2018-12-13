38092c346d3b24a0d3cccb2e047ba9cfca914635
Aaron Paterson <apaterson@protonmail.com>
2018-12-13 00:20:38 -0500

    Pitch globally, yaw locally

--- src/org/reis/game/Player.java
+++ src/org/reis/game/Player.java
@@ -5,19 +5,21 @@ import org.joml.Vector2d;
 import org.joml.Vector3d;
 
 public class Player extends Entity {
-    double speed = 32.0;
+    private double speed = 32.0;
 
     public Player(World world) {
         super(world);
     }
 
     public void walk( Vector3d move ) {
+        // Sense on XZ plane
         double pitch = Math.asin( getOrientation()
             .transformUnitPositiveZ( new Vector3d() )
             .normalize()
             .y()
         );
 
+        // Walk on global XZ plane
         if( move.lengthSquared() != 0 ) act( new Vector3d(move)
             //rotateXYZ(getOrientation())
             .rotate(new Quaterniond(getOrientation()).rotateX(pitch))
@@ -33,8 +35,10 @@ public class Player extends Entity {
         //getOrientation().rotateXYZ(look.y()/1024, look.x()/1024, 0);
         //balance();
 
+        // Global yaw, local pitch
         getOrientation().rotateX( turn.y()/1024 ).rotateLocalY( turn.x()/1024 );
 
+        // Constrain yaw to +-180deg
         double flip = getOrientation().normalizedPositiveY( new Vector3d( ) ).y();
         if ( flip < 0 ) getOrientation().rotateX( Math.copySign(
             Math.asin( flip ),
@@ -42,6 +46,7 @@ public class Player extends Entity {
         ) );
     }
     private void balance( ) {
+        // Roll towards local Y -> global Y
         getOrientation().rotateZ( -Math.asin( getOrientation().transformPositiveX( new Vector3d( ) ).y() ) );
     }
 }
